7e7f3667d5dfb78f9fa8cc6dca59d3d50fc08175
okauppinen <olli.kauppinen@nls.fi>
2018-11-20 16:29:43 +0200

    use arrow function

--- instance.js
+++ instance.js
@@ -93,7 +93,6 @@ Oskari.clazz.define('Oskari.mapframework.bundle.terrain-profile.TerrainProfileBu
          * @method doQuery send query to backend
          */
         doQuery: function () {
-            var me = this;
             if (!this.feature) {
                 return;
             }
@@ -104,8 +103,8 @@ Oskari.clazz.define('Oskari.mapframework.bundle.terrain-profile.TerrainProfileBu
                 dataType: 'json',
                 url: url,
                 success: this.showFlyout.bind(this),
-                error: function (jqXHR, textStatus, errorThrown) {
-                    me.flyout.showError();
+                error: (jqXHR, textStatus, errorThrown) => {
+                    this.flyout.showError();
                     Oskari.log('TerrainProfile').warn('Could not load terrain profile data: ' + errorThrown);
                 }
             });
