a973ced36ef3e45eaeadb89a4e6b270175acfee5
jheijari <joonas.heijari@gmail.com>
2019-05-15 13:25:59 +0300

    Russian translations

---
+++ bundles/asdi/asdi-login/resources/locale/ru.js
@@ -0,0 +1,17 @@
+Oskari.registerLocalization(
+{
+    "lang": "ru",
+    "key": "asdi-login",
+    "value": {
+        "title": "Авторизация",
+        "desc": "Авторизация ИПД Арктики",
+        "flyout": {
+            "title": "Авторизация",
+            "username": "Имя пользователя",
+            "password": "Пароль",
+            "login": "Авторизация",
+            "cancel": "Отменить",
+            "passwdreset": "Забыли пароль?"
+        }
+    }
+});
