8e37a898bc64733b3b160664929d3f876328f54c
okauppinen <olli.kauppinen@nls.fi>
2023-08-08 11:37:39 +0300

    register model

--- bundles/mapping/mapanalysis/plugin/AnalysisLayerPlugin.ol.js
+++ bundles/mapping/mapanalysis/plugin/AnalysisLayerPlugin.ol.js
@@ -13,13 +13,9 @@ Oskari.clazz.define(
      *
      */
 
-    function () {
-        var me = this;
-        me.ajaxUrl = null;
-        if (me._config && me._config.ajaxUrl) {
-            me.ajaxUrl = me._config.ajaxUrl;
-        }
-        this._log = Oskari.log(this.getName());
+    function (config) {
+        this.ajaxUrl = config?.ajaxUrl || Oskari.urls.getRoute('GetAnalysis') + '?';
+        this.loc = Oskari.getMsg.bind(null, 'MapAnalysis');
     }, {
         __name: 'AnalysisLayerPlugin',
         _clazz: 'Oskari.mapframework.bundle.mapanalysis.plugin.AnalysisLayerPlugin',
@@ -35,104 +31,54 @@ Oskari.clazz.define(
          * Interface method for the module protocol.
          */
         _initImpl: function () {
-            const layerClass = 'Oskari.mapframework.bundle.mapanalysis.domain.AnalysisLayer';
-            const modelBuilderClass = 'Oskari.mapframework.bundle.mapanalysis.domain.AnalysisLayerModelBuilder';
-            const layerModelBuilder = Oskari.clazz.create(modelBuilderClass, this.getSandbox());
-
+            const type = this.getLayerTypeSelector();
+            const options = {
+                type,
+                ...this.loc('layer'),
+                createTools: [layer => this._createFilterTool(layer)]
+            };
+            this.getSandbox().getService('Oskari.mapframework.service.MapLayerService')?.registerLayerForUserDataModelBuilder(options);
+            // Let wfs plugin handle this layertype
             const wfsPlugin = this.getMapModule().getLayerPlugins('wfs');
-            if (typeof wfsPlugin.registerLayerType === 'function') {
-                // Let wfs plugin handle this layertype
-                wfsPlugin.registerLayerType(this.layertype, layerClass, layerModelBuilder);
-                this.unregister();
-                return;
-            }
-            // register domain builder
-            const mapLayerService = this.getSandbox().getService('Oskari.mapframework.service.MapLayerService');
-            if (!mapLayerService) {
-                return;
-            }
-            mapLayerService.registerLayerModel(this.layertype, layerClass);
-            mapLayerService.registerLayerModelBuilder(this.layertype, layerModelBuilder);
+            wfsPlugin.registerLayerType(type, 'Oskari.mapframework.bundle.mapanalysis.domain.AnalysisLayer');
+            this.unregister();
         },
-
-        /**
-         * @private @method _startPluginImpl
-         * Interface method for the plugin protocol.
-         */
-        _startPluginImpl: function () {
-            if (!this.ajaxUrl) {
-                this.ajaxUrl =
-                    Oskari.urls.getRoute('GetAnalysis') + '?';
-            }
-        },
-
-        /**
-         * Adds a single Analysis layer to this map
-         *
-         * @method addMapLayerToMap
-         * @param {Oskari.mapframework.bundle.mapanalysis.domain.AnalysisLayer} layer
-         * @param {Boolean} keepLayerOnTop
-         * @param {Boolean} isBaseMap
-         */
-        addMapLayerToMap: function (layer, keepLayerOnTop, isBaseMap) {
-            var me = this,
-                imgUrl = layer.getWpsUrl() + layer.getWpsLayerId(),
-                wms = {
-                    'URL': imgUrl,
-                    'LAYERS': layer.getWpsName(),
-                    'FORMAT': 'image/png'
-                },
-                visible = layer.isInScale(this.getMapModule().getMapScale()) && layer.isVisible(),
-                opacity = layer.getOpacity() / 100,
-                openlayer = new olLayerImage({
-                    source: new olSourceImageWMS({
-                        url: wms.URL,
-                        params: {
-                            'LAYERS': wms.LAYERS,
-                            'FORMAT': wms.FORMAT
-                        },
-                        crossOrigin: layer.getAttributes('crossOrigin')
-                    }),
-                    visible: visible,
-                    opacity: opacity
+        _createFilterTool: function (layer) {
+            const filterdataTool = Oskari.clazz.create('Oskari.mapframework.domain.Tool');
+            filterdataTool.setName('filterdata');
+            filterdataTool.setIconCls('show-filter-tool');
+            filterdataTool.setTooltip(this.loc('filterTooltip'));
+            filterdataTool.setCallback(() => {
+                const service = this.getSandbox().getService('Oskari.analysis.bundle.analyse.service.AnalyseService');
+                const isAggregateValueAvailable = !!service;
+                const fixedOptions = {
+                    bboxSelection: true,
+                    clickedFeaturesSelection: false,
+                    addLinkToAggregateValues: isAggregateValueAvailable
+                };
+    
+                const filterDialog = Oskari.clazz.create('Oskari.userinterface.component.FilterDialog', fixedOptions);
+                filterDialog.setUpdateButtonHandler((filters) => {
+                    // throw event to new wfs
+                    const filteringChangeEvent = Oskari.eventBuilder('WFSSetPropertyFilter');
+                    this.sandbox.notifyAll(filteringChangeEvent(filters, layer.getId()));
                 });
-
-            const zoomLevelHelper = getZoomLevelHelper(this.getMapModule().getScaleArray());
-            // Set min max zoom levels that layer should be visible in
-            zoomLevelHelper.setOLZoomLimits(openlayer, layer.getMinScale(), layer.getMaxScale());
-
-            this._registerLayerEvents(openlayer, layer);
-            this.getMapModule().addLayer(openlayer, !keepLayerOnTop);
-
-            // store reference to layers
-            this.setOLMapLayers(layer.getId(), openlayer);
-
-            me._log.debug(
-                '#!#! CREATED OPENLAYER.LAYER.WMS for AnalysisLayer ' +
-                layer.getId()
-            );
-        },
-        /**
-         * Adds event listeners to ol-layers
-         * @param {OL3 layer} layer
-         * @param {Oskari layerconfig} oskariLayer
-         *
-         */
-        _registerLayerEvents: function (layer, oskariLayer) {
-            var me = this;
-            var source = layer.getSource();
-
-            source.on('imageloadstart', function () {
-                me.getMapModule().loadingState(oskariLayer.getId(), true);
+    
+                if (isAggregateValueAvailable) {
+                    const aggregateAnalyseFilter = Oskari.clazz.create('Oskari.analysis.bundle.analyse.aggregateAnalyseFilter', filterDialog);
+    
+                    filterDialog.createFilterDialog(layer, null, () =>  {
+                        // FIXME: why is bind() used here? can it be removed?
+                        service._returnAnalysisOfTypeAggregate((listOfAggregateAnalysis) => aggregateAnalyseFilter.addAggregateFilterFunctionality(listOfAggregateAnalysis));
+                    });
+                } else {
+                    filterDialog.createFilterDialog(layer);
+                }
+                filterDialog.setCloseButtonHandler(() => {
+                    filterDialog.popup.dialog.off('click', '.add-link');
+                });
             });
-
-            source.on('imageloadend', function () {
-                me.getMapModule().loadingState(oskariLayer.getId(), false);
-            });
-
-            source.on('imageloaderror', function () {
-                me.getMapModule().loadingState(oskariLayer.getId(), null, true);
-            });
+            return filterdataTool;
         }
     }, {
         'extend': ['Oskari.mapping.mapmodule.AbstractMapLayerPlugin'],
